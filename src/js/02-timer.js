89230cd1a5df07719a601011473c81472fb1ae79
BogdanV <zarazailo@gmail.com>
2022-03-19 21:40:45 +0200

    f

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -71,14 +71,14 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // days
-  const days = Math.floor(ms / day);
-  // hours
-  const hours = Math.floor((ms % day) / hour);
-  // minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+   // Remaining days
+   const days = Math.floor(ms / day);
+   // Remaining hours
+   const hours = Math.floor((ms % day) / hour);
+   // Remaining minutes
+   const minutes = Math.floor(((ms % day) % hour) / minute);
+   // Remaining seconds
+   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 };
